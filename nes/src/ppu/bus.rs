dee1279512ee1798b768756677cb540524792d28
relam <relam@rcschools.us>
2021-05-18 11:24:36 -0400

    refactor ppu bus

--- nes/src/ppu/bus.rs
+++ nes/src/ppu/bus.rs
@@ -24,6 +24,7 @@ pub struct Bus {
     pinout: Pinout,
     rd_buffer: Option<u8>,
     wr_buffer: Option<u8>,
+    palette_write: bool,
 }
 
 impl Bus {
@@ -32,126 +33,98 @@ impl Bus {
             pinout: Pinout::new(),
             rd_buffer: Some(0),
             wr_buffer: None,
+            palette_write: false,
         }
     }
 
     pub fn io_read(&mut self) -> u8{
-        self.io_action = IOAction::LatchRead;
-        self.rd_buffer
+        self.rd_buffer.take().unwrap()
     }
 
     pub fn io_write(&mut self, data: u8) {
-        self.io_action = IOAction::LatchWrite;
-        self.wr_buffer = data;
+        self.wr_buffer = Some(data);
     }
 
-    pub fn read(&mut self, mapper: &mut dyn Mapper, address: u16) {
-        
+    pub fn io_palette_read(&mut self) {
+        self.rd_buffer = None;
     }
 
-    pub fn execute(&mut self, mapper: &mut dyn Mapper, render_action: RenderAction, mut pinout: Pinout) -> Pinout { 
-        // clear ctrl pins
-        pinout.ctrl.set(Ctrl::RD, true);
-        pinout.ctrl.set(Ctrl::WR, true);
-        pinout.ctrl.set(Ctrl::ALE, false);
-        self.io_mem_access = false;
+    pub fn io_palette_write(&mut self) {
+        self.palette_write = true;
+    }
 
-        match (render_action, self.io_action) {
-            (RenderAction::Idle, IOAction::Idle) => { }
-            (RenderAction::Idle, IOAction::LatchRead) => {
-                pinout.ctrl.set(Ctrl::ALE, true);
-                self.internal_capture_latch(pinout);
-                self.io_action = IOAction::Read;
+    pub fn latch(&mut self, mapper: &mut dyn Mapper, address: u16) {
+        self.pinout.ctrl.set(Ctrl::WR, false);
+        self.pinout.ctrl.set(Ctrl::RD, false);
+        self.pinout.ctrl.set(Ctrl::ALE, true);
+    }
+
+    pub fn read(&mut self, mapper: &mut dyn Mapper, address: u16) -> (u8, bool) {
+        self.pinout.ctrl.set(Ctrl::WR, false);
+        self.pinout.ctrl.set(Ctrl::RD, false);
+        self.pinout.ctrl.set(Ctrl::ALE, false);
+
+        self.pinout.address = address;
+
+        match self.wr_buffer.take() {
+            Some(data) => {
+                // if write buffer was filled then perform write instead
+                self.pinout.data = data;
+                self.pinout = self.internal_write(mapper, self.pinout);
+                return (data, true);
             }
-            (RenderAction::Idle, IOAction::LatchWrite) => {
-                pinout.ctrl.set(Ctrl::ALE, true);
-                self.internal_capture_latch(pinout);
-                self.io_action = IOAction::Write;
-            }
-            (RenderAction::Idle, IOAction::Read) => {
-                pinout.ctrl.set(Ctrl::RD, false);
-                pinout = self.internal_read(mapper, pinout);
-                self.rd_buffer = pinout.data;
-                self.io_mem_access = true;
-                self.io_action = IOAction::Idle;
-            }
-            (RenderAction::Idle, IOAction::Write) => {
-                pinout.ctrl.set(Ctrl::WR, false);
-                pinout = self.internal_write(mapper, pinout);
-                self.io_mem_access = true;
-                self.io_action = IOAction::Idle;
-            }
-            (RenderAction::Latch, IOAction::Idle) => {
-                pinout.ctrl.set(Ctrl::ALE, true);
-                self.internal_capture_latch(pinout);
-            }
-            (RenderAction::Latch, IOAction::LatchRead) => {
-                pinout.ctrl.set(Ctrl::ALE, true);
-                self.internal_capture_latch(pinout);
-                self.io_action = IOAction::Read;
-            }
-            (RenderAction::Latch, IOAction::LatchWrite) => {
-                pinout.ctrl.set(Ctrl::ALE, true);
-                self.internal_capture_latch(pinout);
-                self.io_action = IOAction::Write;
-            }
-            (RenderAction::Latch, IOAction::Read) => {
-                pinout.ctrl.set(Ctrl::RD, false);
-                pinout.ctrl.set(Ctrl::ALE, true);
-                pinout = self.internal_read(mapper, pinout);
-                self.internal_capture_latch_io_during_render(pinout);
-                self.rd_buffer = pinout.data;
-                self.io_action = IOAction::Idle;
-                self.io_mem_access = true;
-            }
-            (RenderAction::Latch, IOAction::Write) => {
-                pinout.ctrl.set(Ctrl::ALE, true);
-                pinout.ctrl.set(Ctrl::WR, false);
-                pinout = self.internal_write(mapper, pinout);
-                self.internal_capture_latch_io_during_render(pinout);
-                self.io_action = IOAction::Idle;
-                self.io_mem_access = true;
-            }
-            (RenderAction::Read, IOAction::Idle) => {
-                pinout.ctrl.set(Ctrl::RD, false);
-                pinout = self.internal_read(mapper, pinout);
-            }
-            (RenderAction::Read, IOAction::LatchRead) => {
-                pinout.ctrl.set(Ctrl::RD, false);
-                pinout.ctrl.set(Ctrl::ALE, true);
-                pinout = self.internal_read(mapper, pinout);
-                self.internal_capture_latch_io_during_render(pinout);
-                self.io_action = IOAction::Read;
-            }
-            (RenderAction::Read, IOAction::LatchWrite) => {
-                pinout.ctrl.set(Ctrl::RD, false);
-                pinout.ctrl.set(Ctrl::ALE, true);
-                pinout = self.internal_read(mapper, pinout);
-                self.internal_capture_latch_io_during_render(pinout);
-                self.io_action = IOAction::Write;
-            }
-            (RenderAction::Read, IOAction::Read) => {
-                pinout.ctrl.set(Ctrl::RD, false);
-                pinout = self.internal_read(mapper, pinout);
-                self.rd_buffer = pinout.data;
-                self.io_action = IOAction::Idle;
-                self.io_mem_access = true;
-            }
-            (RenderAction::Read, IOAction::Write) => {
-                pinout.ctrl.set(Ctrl::RD, false);
-                pinout.ctrl.set(Ctrl::WR, false);
-                pinout = self.internal_write(mapper, pinout);
-                self.io_action = IOAction::Idle;
-                self.io_mem_access = true;
+            None => { }
+        }
+
+        self.pinout = self.internal_read(mapper, self.pinout);
+
+        match self.rd_buffer {
+            Some(_) => { }
+            None => { 
+                self.rd_buffer = Some(self.pinout.data);
+                return (self.pinout.data, true);
             }
         }
 
-        pinout
+        let tmp = self.palette_write;
+        self.palette_write = false;
+
+        (self.pinout.data, tmp)        
+    }
+
+    pub fn idle(&mut self, mapper: &mut dyn Mapper, address: u16) -> bool {
+        self.pinout.ctrl.set(Ctrl::WR, false);
+        self.pinout.ctrl.set(Ctrl::RD, false);
+        self.pinout.ctrl.set(Ctrl::ALE, false);
+
+        self.pinout.address = address;
+
+        match self.wr_buffer.take() {
+            Some(data) => {
+                // if write buffer was filled then perform write instead
+                self.pinout.data = data;
+                self.pinout = self.internal_write(mapper, self.pinout);
+                return true;
+            }
+            None => { }
+        }
+
+        match self.rd_buffer {
+            Some(_) => { }
+            None => { 
+                self.pinout = self.internal_read(mapper, self.pinout);
+                self.rd_buffer = Some(self.pinout.data);
+                return true
+            }
+        }
+
+        let tmp = self.palette_write;
+        self.palette_write = false;
+        tmp
     }
 
     fn internal_read(&mut self, mapper: &mut dyn Mapper,  mut pinout: Pinout) -> Pinout {
-        pinout = self.internal_apply_latch(pinout);
-
+        pinout.ctrl.set(Ctrl::RD, true);
         match pinout.address {
             0x0000..=0x1fff => { pinout = mapper.read_ppu_chr(pinout); }
             0x2000..=0x3fff => { pinout = mapper.read_ppu_nt(pinout); }
@@ -162,30 +135,13 @@ impl Bus {
     }
 
     fn internal_write(&mut self, mapper: &mut dyn Mapper,  mut pinout: Pinout) -> Pinout {
-        // rendering never writes
-        pinout.data = self.wr_buffer;
-        pinout = self.internal_apply_latch(pinout);
-
-        match pinout.address {
+        pinout.ctrl.set(Ctrl::WR, true);
+         match pinout.address {
             0x0000..=0x1fff => { pinout = mapper.write_ppu_chr(pinout); }
             0x2000..=0x3fff => { pinout = mapper.write_ppu_nt(pinout); }
             _ => panic!("ppu write {:#X} - should not be able to read past 0x2fff during rendering", pinout.address)
         }
 
-        pinout
-    }
-
-    fn internal_capture_latch(&mut self, pinout: Pinout) {
-        self.latch = pinout.address as u8;
-    }
-
-    fn internal_capture_latch_io_during_render(&mut self, pinout: Pinout) {
-        // cause when conflict between io and render latch/reads
-        self.latch = pinout.data;
-    }
-
-    fn internal_apply_latch(&mut self, mut pinout: Pinout) -> Pinout {
-        pinout.address = (pinout.address & 0xFF00) | (self.latch as u16);
         pinout
     }
 }
@@ -196,23 +152,18 @@ mod test {
     use crate::mappers::mapper_debug::MapperDebug;
 
     #[test]
-    fn test_render_access() {
-        let mut p = Pinout::new();
+    fn test_read_access() {
         let mut mapper = MapperDebug::new();
         let mut bus = Bus::new();
 
         mapper.poke_chr(0x1FF, 255);
-        p.address = 0x1FF;
 
-        p = bus.execute(&mut mapper, RenderAction::Latch, p);
-        assert_eq!(p.address, 0x1FF);
-        p = bus.execute(&mut mapper, RenderAction::Read, p);
-        assert_eq!(255, p.data);
+        let (d, b) = bus.read(&mut mapper, 0x1FF);
+        assert_eq!(255, d);
     }
 
     #[test]
-    fn test_io_access() {
-        let mut p = Pinout::new();
+    fn test_io_during_read() {
         let mut mapper = MapperDebug::new();
         let mut bus = Bus::new();
 
